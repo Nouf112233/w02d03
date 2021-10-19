1e56a07afb26e69fc9316c9c37b05fa64b660e79
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-19 11:36:08 +0300

    edit

--- task1/cars.js
+++ task1/cars.js
@@ -19,7 +19,11 @@ showAll()
 }
 
 const car1 = new Cars("toyata", 2020, "white", 112233, '8gjfi', 70000);
-const car1 = new Cars("houandai", 2018, "black", 73635, 'du8787', 100000);
-const car1 = new Cars("ford", 2000, "blue", 112233, 'hd65d', 200000);
-const car1 = new Cars("toyata", 1017, "black", 765408, '876dj', 99000);
-const car1 = new Cars("mazda", 2021, "red", 635330, '76ff8d', 300000);
+const car2 = new Cars("houandai", 2018, "black", 73635, 'du8787', 100000);
+const car3 = new Cars("ford", 2000, "blue", 112233, 'hd65d', 200000);
+const car4 = new Cars("toyata", 1017, "black", 765408, '876dj', 99000);
+const car5 = new Cars("mazda", 2021, "red", 635330, '76ff8d', 300000);
+
+car1.editPrice(80000);
+car1.showAll();
+
