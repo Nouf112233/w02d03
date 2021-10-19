b3789995eab6cddb2165fb70652413971e1ad9d3
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-19 11:24:06 +0300

    add task1

---
+++ task1/cars.js
@@ -0,0 +1,25 @@
+class Cars{
+    Carsconstructor(make, model, colour, image, registNumber, price) {
+    this.make = make;
+    this.model = model;
+    this.colour =colour;
+    this.image=image;
+    this.registNumber=registNumber;
+    this.price=price;
+    
+};
+editPrice(newPrice)
+{
+    this.price=newPrice;
+}
+showAll()
+{
+    console.log(this);
+}
+}
+
+const car1 = new Cars("toyata", 2020, "white", 112233, '8gjfi', 70000);
+const car1 = new Cars("houandai", 2018, "black", 73635, 'du8787', 100000);
+const car1 = new Cars("ford", 2000, "blue", 112233, 'hd65d', 200000);
+const car1 = new Cars("toyata", 1017, "black", 765408, '876dj', 99000);
+const car1 = new Cars("mazda", 2021, "red", 635330, '76ff8d', 300000);
